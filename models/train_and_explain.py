01f964f48fa068a4cc53944e7a7a56f91d7b28c4
peniel18 <penieletornam18@gmail.com>
2026-07-18 17:17:05 +0000

    dir issues fixed

--- models/train_and_explain.py
+++ models/train_and_explain.py
@@ -284,7 +284,7 @@ if __name__ == "__main__":
     print("\nLoading dataset ...")
     train_loader, val_loader, test_loader, dataset = load_real_data(
         cif_dir    = "../data/data/cif_files/",
-        label_csv  = "../data/label_template.csv",
+        label_csv  = "inverse-design-of-Perovskite-using-LLM/data/label_template.csv",
         cache_dir  = "../data/data/graph_cache",
         batch_size = CFG["batch_size"],
     )
